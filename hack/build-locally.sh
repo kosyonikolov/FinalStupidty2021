6dc256260a816f59f344c2c90010863114993767
Ivan Chuchulski <zake6@abv.bg>
2021-07-01 14:42:36 +0300

    add script for local pdf building

---
+++ hack/build-locally.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+outdir=build-$(date +"%y-%m-%d")
+
+mkdir ${outdir}
+
+for FILE in $(find . -type f -name '*.tex')
+do
+    tex_file=$(basename ${FILE})
+    result_pdf=$(echo ${tex_file} | sed "s/tex/pdf/g")
+
+    pushd $(dirname ${FILE}) &> /dev/null
+        echo "begin building ${tex_file}..."
+        latexmk -interaction=nonstopmode -pdf ${tex_file} &> /dev/null
+        latexmk -c &> /dev/null
+        mv ${result_pdf} ../${outdir}/$(dirname ${FILE})_${result_pdf}
+        echo "done building ${tex_file}..."
+    popd &> /dev/null
+    echo ""
+done
+
+echo "build is done"
